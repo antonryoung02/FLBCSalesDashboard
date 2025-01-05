f96cb66e14a1aa26a44d3992f044221db9cb1b04
= <anton@wustl.edu>
2025-01-05 15:04:37 -0700

    Testing update script with new package

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+import dotenv
 BASE_DIR_PATH = os.path.join(os.path.expanduser("~"), "OneDrive", "Desktop", "FLBC 1.1.1", "Revenue and menu engineering data")
 CONFIG_SHEET_NAME = "CONFIG"
 DATA_FILENAME = "MENU ENGINEERING FLBC PBHS and CELLAR.xlsx"
